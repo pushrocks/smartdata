3ba2bc8446af271acc5770bfe9c086ad1615195d
Phil Kunz <gitlab@philkunz.com>
2018-01-07 14:45:43 +0100

    add rethink

--- ts/smartdata.plugins.ts
+++ ts/smartdata.plugins.ts
@@ -2,13 +2,13 @@ import 'typings-global'
 import * as assert from 'assert'
 import * as beautylog from 'beautylog'
 import * as lodash from 'lodash'
-import * as mongodb from 'mongodb'
+import * as rethinkDb from 'rethinkdb'
 import * as smartq from 'smartq'
 
 export {
     assert,
     beautylog,
     lodash,
-    mongodb,
-    smartq
+    smartq,
+    rethinkDb
 }
